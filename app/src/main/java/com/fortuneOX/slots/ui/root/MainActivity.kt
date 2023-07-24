131f417c29aecd67ddc1e81b5c0de61bf8956f49
Oduvan58 <yancheuskipavel@gmail.com>
2023-07-24 11:58:55 +0300

    Initial commit

---
+++ app/src/main/java/com/fortuneOX/slots/ui/root/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.fortuneOX.slots.ui.root
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fortuneOX.slots.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
