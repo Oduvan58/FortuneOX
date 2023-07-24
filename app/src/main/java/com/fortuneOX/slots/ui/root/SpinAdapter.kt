3dc871d687e41896a9038052e5ea8e1da5b9eb6d
nikitabranovitski <nikita.branovitski1999@gmail.com>
2023-07-24 15:09:32 +0300

    spin screen

---
+++ app/src/main/java/com/fortuneOX/slots/ui/root/SpinAdapter.kt
@@ -0,0 +1,41 @@
+package com.fortuneOX.slots.ui.root
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.fortuneOX.slots.R
+import com.fortuneOX.slots.databinding.SpinListItemBinding
+
+class SpinAdapter: ListAdapter<Int, SpinAdapter.SpinViewHolder>(SpinDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpinViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemBinding = SpinListItemBinding.inflate(inflater, parent, false)
+        return SpinViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: SpinViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class SpinViewHolder(private val binding: SpinListItemBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Int){
+            binding.spinItemImageView.setImageResource(item)
+        }
+    }
+
+
+}
+
+class SpinDiffCallback : DiffUtil.ItemCallback<Int>() {
+
+    override fun areItemsTheSame(oldItem: Int, newItem: Int) = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: Int, newItem: Int) =
+        oldItem == newItem
+
+}
---
+++ app/src/main/java/com/fortuneOX/slots/ui/root/SpinFragment.kt
@@ -0,0 +1,52 @@
+package com.fortuneOX.slots.ui.root
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fortuneOX.slots.R
+import com.fortuneOX.slots.databinding.FragmentSpinBinding
+
+class SpinFragment : Fragment() {
+
+    private lateinit var binding: FragmentSpinBinding
+
+    private val spinItems = listOf(
+        R.drawable.orange,
+        R.drawable.orange,
+        R.drawable.orange,
+        R.drawable.orange,
+        R.drawable.orange,
+        R.drawable.orange,
+    )
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSpinBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupSpinAdapter()
+    }
+
+    private fun setupSpinAdapter() {
+        val spinAdapter = SpinAdapter()
+        binding.firstList.layoutManager = LinearLayoutManager(requireContext())
+        binding.secondList.layoutManager = LinearLayoutManager(requireContext())
+        binding.thirdList.layoutManager = LinearLayoutManager(requireContext())
+        binding.fourthList.layoutManager = LinearLayoutManager(requireContext())
+        binding.firstList.adapter = spinAdapter
+        binding.secondList.adapter = spinAdapter
+        binding.thirdList.adapter = spinAdapter
+        binding.fourthList.adapter = spinAdapter
+       spinAdapter.submitList(spinItems)
+    }
+
+}
